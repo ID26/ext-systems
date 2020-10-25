9e6e5e4a6dc71e73587a3b1e8741dbef09ffab2b
Ivan Denisov <workpost26@gmail.com>
2020-10-25 16:07:32 +0300

    Request 1

---
+++ city-register/src/main/java/edu/javacourse/city/domain/PersonRequest.java
@@ -0,0 +1,72 @@
+package edu.javacourse.city.domain;
+
+import java.time.LocalDate;
+
+public class PersonRequest {
+    private String serName;
+    private String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+    private String building;
+    private String extension;
+    private String apartment;
+
+    public String getSerName() {
+        return serName;
+    }
+
+    public void setSerName(String serName) {
+        this.serName = serName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+
+
+}
