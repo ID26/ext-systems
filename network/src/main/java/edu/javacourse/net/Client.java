4df882be6a082c23f40e2a998bca8759ed630949
Ivan Denisov <workpost26@gmail.com>
2020-10-29 01:48:17 +0300

    Soft Set Server

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -6,18 +6,23 @@ import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 500; i++) {
-            SimpleClient sc = new SimpleClient();// создаем отдельный объект (поток-нить (thread))
+        for (int i = 0; i < 8; i++) {
+            SimpleClient sc = new SimpleClient(i);// создаем отдельный объект (поток-нить (thread))
             sc.start(); // вызываем метод старт java машина запускает поток на отдельном ядре
         }
     }
 }
 
     class SimpleClient extends Thread {
+    public static final String[] COMMAND={"HELLO", "MORNING", "DAY", "EVENING"};
+    private int cmdNumber;
+    public SimpleClient (int cmdNumber){
+        this.cmdNumber=cmdNumber;
+    }
         @Override
         public void run() {
             try {
-                System.out.println("Started: " + LocalDateTime.now()); //print start thread
+//                System.out.println("Started: " + LocalDateTime.now()); //print start thread
                 Socket socket = new Socket("127.0.0.1", 25225);// ip address and port
                 // на вход потока, преобразующего символы в слова, подаем поток,
                 // преобразующий байты в символ, на вход которого передаем входной поток байтов
@@ -26,7 +31,10 @@ public class Client {
                 // преобразующий символы в байты, на вход которого передаем выходной поток байтов
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-                String sb = "Ivan";
+                String command=COMMAND[cmdNumber%COMMAND.length];
+
+
+                String sb = command+" "+"Ivan";
                 bw.write(sb);// send result
                 bw.newLine();
                 bw.flush();//принудительное отправление
@@ -37,7 +45,7 @@ public class Client {
                 //закрываем потоки
                 br.close();
                 bw.close();
-                System.out.println("Finished: " + LocalDateTime.now()); //print finish thread
+//                System.out.println("Finished: " + LocalDateTime.now()); //print finish thread
 
             } catch (IOException ex) {
                 ex.printStackTrace(System.out);
