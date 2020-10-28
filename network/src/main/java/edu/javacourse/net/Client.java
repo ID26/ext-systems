7fb91b5039b2a4d96434405c03939f4999b02b63
Ivan Denisov <workpost26@gmail.com>
2020-10-28 20:02:08 +0300

    Stream

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -1,2 +1,34 @@
-package edu.javacourse.net;public class Client {
+package edu.javacourse.net;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 10; i++) {
+            sendRequest();
+        }
+    }
+
+    private static void sendRequest() throws IOException {
+        Socket socket=new Socket("127.0.0.1", 25225);// ip address and port
+        // на вход потока, преобразующего символы в слова, подаем поток,
+        // преобразующий байты в символ, на вход которого передаем входной поток байтов
+        BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // на выходной поток, преобразующего слова в символы, подаем поток,
+        // преобразующий символы в байты, на вход которого передаем выходной поток байтов
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        String sb="Ivan";
+        bw.write(sb);// send result
+        bw.newLine();
+        bw.flush();//принудительное отправление
+
+        String answer = br.readLine();
+        System.out.println("Client got string: "+answer);
+
+        //закрываем потоки
+        br.close();
+        bw.close();
+    }
 }
--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -1,2 +1,44 @@
-package edu.javacourse.net;public class Server {
+package edu.javacourse.net;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket=new ServerSocket(25225); //Локальный порт передаем
+
+        System.out.println("Server is started!!!");
+
+        while (true){
+            Socket client= socket.accept(); //приходит сокет со входным и выходным потоками
+            handleRequest(client);
+        }
+    }
+
+    private static void handleRequest(Socket client) throws IOException {
+        // на вход потока, преобразующего символы в слова, подаем поток,
+        // преобразующий байты в символ, на вход которого передаем входной поток байтов
+        BufferedReader br=new BufferedReader(new InputStreamReader(client.getInputStream()));
+        // на выходной поток, преобразующего слова в символы, подаем поток,
+        // преобразующий символы в байты, на вход которого передаем выходной поток байтов
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+        //ДЛЯ ТОГО, чтоб видоизменять строку не дорого для памяти,
+        // просто стринг каждый раз создает новый объект
+        StringBuilder sb=new StringBuilder("Hallow, ");
+        String userName=br.readLine();
+        System.out.println("Server got string: "+userName);
+        sb.append(userName);// к нашей строке прибавляем полученное из потока
+        bw.write(sb.toString());// send result
+        bw.newLine();
+        bw.flush();//принудительное отправление
+
+        //закрываем потоки
+        br.close();
+        bw.close();
+
+        //закрываем соккет
+        client.close();
+    }
 }
